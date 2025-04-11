9035dfffb9df7146ba94389b2c98a97a111005c2
AyushaJadhav <130702125+AyushaJadhav@users.noreply.github.com>
2025-04-11 16:23:23 +0530

    Update kiteTicker.js

--- backend/kiteTicker.js
+++ backend/kiteTicker.js
@@ -6,9 +6,13 @@ const subscribeSSE = (req, res) => {
   const sendData = () => {
     const mockData = JSON.stringify([{ 
       timestamp: new Date(), 
-      ohlc: { open: 100, high: 105, low: 95, close: 100 } 
+      ohlc: { open: 100, high: 105, low: 95, close: 98 },
+      "index": 102.5,
+    "atmStrike": 101,
+    "vwap": 100.8 
     }]);
-    res.write(`data: ${mockData}\n\n`);
+    res.write(`data: ${JSON.stringify([tick])}\n\n`);
+
   };
 
   const interval = setInterval(sendData, 2000);
